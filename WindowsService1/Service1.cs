8d5e8fddff7a85c8021d4ead446b59b9c62c1383
NeilOCV <31700601+NeilOCV@users.noreply.github.com>
2017-09-20 08:44:21 +0200

    First check in

---
+++ WindowsService1/Service1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+
+namespace WindowsService1
+{
+    public partial class Service1 : ServiceBase
+    {
+        public Service1()
+        {
+            InitializeComponent();
+        }
+        public void OnDebug()
+        {
+            OnStart(null);
+        }
+        protected override void OnStart(string[] args)
+        {
+            System.IO.File.Create(@"C:\Service\Test\" + string.Format("{0:yyyyMMddhhmmss}", DateTime.Now) + " - Start.txt");
+        }
+
+        protected override void OnStop()
+        {
+            System.IO.File.Create(@"C:\Service\Test\" + string.Format("{0:yyyyMMddhhmmss}", DateTime.Now) + " - Stop.txt");
+        }
+    }
+}
